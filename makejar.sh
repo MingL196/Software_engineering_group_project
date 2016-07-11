96dd7b7fbc2dd725e73d75088ca922948f3d7cd3
hmmmn1 <ubuntumemory@gmail.com>
2016-07-11 10:07:01 -0500

    Mon Jul 11 10:07:01 EST 2016

--- makejar.sh
+++ makejar.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf *.class; rm -rf *.jar
 javac Main.java
-#jar cfm triviopoly.jar manifest.mf *.class *.java default_settings.txt README
-jar cfm triviopoly.jar manifest.mf *.class default_settings.txt
+jar cfm triviopoly.jar manifest.mf *.class *.java default_settings.txt README
+#jar cfm triviopoly.jar manifest.mf *.class default_settings.txt
